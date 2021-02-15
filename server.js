35042e2515e5f55f8f8e4b0f2070714b7a7854c8
techiesureshkumar <74500188+techiesureshkumar@users.noreply.github.com>
2021-02-15 16:45:56 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,45 @@
+let express = require('express');
+let mongoose = require('mongoose');
+let cors = require('cors');
+let bodyParser = require('body-parser');
+let dbConfig = require('./database/db');
+
+// Express Route
+const studentRoute = require('../backend/routes/student.route');
+
+//Connecting mongoDB Database
+mongoose.Promise = global.Promise;
+//mongoose.connect('mongodb://user:password@domain.com:27017/dbname', { useNewUrlParser: true });
+mongoose.connect(dbConfig.db, {
+    useNewUrlParser: true
+}).then(() => {
+    console.log('Database sucessfully connected!')
+    },
+    error => {
+        console.log('Could not connect to database : ' + error);
+    }
+)
+
+//Express configuration for result
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cors());
+app.use('/students', studentRoute);
+
+//PORT
+const port = process.env.PORT || 4000;
+const server = app.listen(port, () => {
+    console.log('Connected to port ' + port);
+})
+
+//404 Error
+app.use(( req, res, next ) => {
+    next(createError(404));
+})
+
+app.use(function ( err, req, res, next ) {
+    console.error(err.message);
+    if( !err.statusCode ) err.statusCode = 500;
+    res.status(err.statusCode).send(err.message);
+});
